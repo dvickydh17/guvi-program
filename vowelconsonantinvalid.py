6643b124f6c85c5503c1ed6f7c6123f4ab47af77
dvickydh17 <50619530+dvickydh17@users.noreply.github.com>
2019-05-15 13:10:09 +0530

    Create vowelconsonantinvalid.py

---
+++ vowelconsonantinvalid.py
@@ -0,0 +1,10 @@
+chr=input()
+if chr.isalpha()==True:
+    if(chr=='A' or chr=='a' or chr=='E' or chr=='e' or chr=='I' or chr=='i' or chr=='O' or chr=='o' or chr=='U' or chr=='u'):
+        print("Vowel")
+    else:
+       print("Consonant")
+else:
+   print("invalid")
+        
+        
